93f92f9b80d6ae06cd459d51b17b8e4cad0e81e3
莯凛 <mokurin000@gmail.com>
2026-05-12 19:49:07 +0800

    build: debugging CJK fonts on ColorOS

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-slint::include_modules!();
-
 #[cfg(target_os = "android")]
 #[unsafe(no_mangle)]
 fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn std::error::Error>> {
+    use android_logger::Config;
+
     slint::android::init(app).unwrap();
+
+    android_logger::init_once(Config::default().with_max_level(log::LevelFilter::Debug));
+
+    slint::slint!(
+        export component AppWindow inherits Window {
+            Text {
+                text: "测试";
+            }
+        }
+    );
 
     AppWindow::new()?.run()?;
     Ok(())
