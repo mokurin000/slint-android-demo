bd9201584ea5934ea0da6e9d8a52de37f7a89222
莯凛 <mokurin000@gmail.com>
2026-05-13 02:02:43 +0800

    fix: init logger before app init

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@
 fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn std::error::Error>> {
     use android_logger::Config;
 
+    android_logger::init_once(Config::default().with_max_level(log::LevelFilter::Trace));
     slint::android::init(app).unwrap();
-
-    android_logger::init_once(Config::default().with_max_level(log::LevelFilter::Debug));
 
     slint::slint!(
         export component AppWindow inherits Window {
