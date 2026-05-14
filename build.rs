1197c671dec03fddcf2207bfe48b6e550132d9bf
莯凛 <mokurin000@gmail.com>
2026-05-14 17:46:45 +0800

    feat: add noto sans VF and oppo sans instanced

--- build.rs
+++ build.rs
@@ -1,8 +1,16 @@
 fn main() {
-    slint_build::compile_with_config(
-        "ui/app.slint",
-        slint_build::CompilerConfiguration::new()
-            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
-    )
-    .unwrap();
+    if option_env!("FIXED_OPPO_SANS").is_some() {
+        slint_build::compile_with_config(
+            "ui/fixed-weight.slint",
+            slint_build::CompilerConfiguration::new()
+                .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+        )
+    } else {
+        slint_build::compile_with_config(
+            "ui/app.slint",
+            slint_build::CompilerConfiguration::new()
+                .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+        )
+    }
+    .unwrap()
 }
