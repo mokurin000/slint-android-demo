e015c8ee3575b076dc2418ebceaf9b9a04cad019
莯凛 <mokurin000@gmail.com>
2026-05-15 04:57:15 +0800

    build: support test on desktop

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
+#[cfg(not(target_os = "android"))]
+use slint_build::{CompilerConfiguration, EmbedResourcesKind};
+
 fn main() {
+    #[cfg(target_os = "android")]
     slint_build::compile("ui/app.slint").unwrap();
+
+    #[cfg(not(target_os = "android"))]
+    slint_build::compile_with_config(
+        "ui/desktop.slint",
+        CompilerConfiguration::new().embed_resources(EmbedResourcesKind::AsAbsolutePath),
+    )
+    .unwrap();
 }
--- test_slint.ps1
+++ test_slint.ps1
@@ -3,10 +3,7 @@ $env:SLINT_LIVE_PREVIEW = 1
 while ($true) {
     cargo build --features slint/live-preview
 
-    if (-not $?) {
-        Remove-Item .\target\debug\slint_android_demo.pdb -ErrorAction SilentlyContinue
-    }
-    else {
+    if ($?) {
         break;
     }
 }
