100201406e80907660d1a93d4c4ca78a14d18778
莯凛 <mokurin000@gmail.com>
2026-05-15 19:14:59 +0800

    fix: build.rs condition

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
+use std::env;
+
 #[cfg(not(target_os = "android"))]
 use slint_build::{CompilerConfiguration, EmbedResourcesKind};
 
 fn main() {
-    #[cfg(target_os = "android")]
-    slint_build::compile("ui/app.slint").unwrap();
-
-    #[cfg(not(target_os = "android"))]
-    slint_build::compile_with_config(
-        "ui/desktop.slint",
-        CompilerConfiguration::new().embed_resources(EmbedResourcesKind::AsAbsolutePath),
-    )
-    .unwrap();
+    if env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("android") {
+        slint_build::compile("ui/app.slint").unwrap();
+    } else {
+        slint_build::compile_with_config(
+            "ui/desktop.slint",
+            CompilerConfiguration::new().embed_resources(EmbedResourcesKind::AsAbsolutePath),
+        )
+        .unwrap();
+    }
 }
